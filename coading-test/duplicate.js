64435b71a5b45f1aa6d043dfa2b5a08d0b569ca2
its-kuramoto <dev-kuramoto@gmail.com>
2020-08-11 17:19:35 +0900

    working

--- coading-test/duplicate.js
+++ coading-test/duplicate.js
@@ -1,29 +1,29 @@
 /**
  * srouce numbers
- * @type {number[]} nums
+ * @type {number[]} numbers
 */
-const nums = [1, 2, 3, 4, 4];
+const numbers = [1, 2, 3, 4, 4];
 
 /**
  * return true if nums contains duplicate
  * @param {number[]} nums
  * @return {boolean}
 */
-function hasDuplicate() {
-  const checkedMap = new Map();
+function hasDuplicate(nums) {
+  const map = new Map();
   // step1: single linear scan left -> right, build hashMap as we go
   for (let i = 0; i < nums.length; i++) {
     // step2: if Map contains key(nums[i]), then duplicate found so return true
-    if(checkedMap.has(nums[i])) {
+    if (map.has(nums[i])) {
       return true;
     }
-    checkedMap.set(nums[i], nums[i]);
+    map.set(nums[i], nums[i]);
   }
   // return false
   return false;
 }
 
-console.log(hasDuplicate());
+console.log(hasDuplicate(numbers));
 
 // runtime complexity O(n): Single linear scan of nums, so O(n) = O(n)
 // space complexity O(n): if items in array1 are unique, then hashmap size will be O(n)
--- coading-test/non-repeating.js
+++ coading-test/non-repeating.js
@@ -1,10 +1,38 @@
 /**
  * @type {string} source string
 */
-const str = "leetcoad";
+const str = "";
 
 /**
  * return index of first non-repeating char
  * @param {string} s
  * @return {number} 
 */
+function firstUniqChar(s) {
+  // 一文字しかない場合、0を返す。
+  if (s.length == 1) {
+    return 0;
+  }
+
+  const map = new Map();
+
+  // 文字列を左から右へスキャンする。並行してMapに値を追加していく。
+  for (let i = 0; i < s.length; i++) {
+    if (map.has(s[i])) {
+      map.set(s[i], map.get(s[i]) + 1);
+    } else {
+      map.set(s[i], 1);
+    }
+  }
+  
+  // もう一度文字列を左から右へスキャンし、最初にvalueが1のkeyを返す。
+  for (let i = 0; i < s.length; i++) {
+    if (map.get(s[i]) === 1) {
+      return i;
+    }
+  }
+
+  return -1;
+}
+
+console.group(firstUniqChar(str));
